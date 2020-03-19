81925754049ff5e9107d510b73e43890da5f2265
Carlos21122000 <53351491+Carlos21122000@users.noreply.github.com>
2020-03-19 10:41:33 -0600

    Update Patrones_Lexer.java

--- Proyecto1/src/Patrones_Lexer.java
+++ Proyecto1/src/Patrones_Lexer.java
@@ -14,17 +14,17 @@ class Patrones_Lexer{
         public static final String CAR = "car";
         //Devuelve la lista sin su primer elemento 
         public static final String CDR = "cdr";
-        //La palabra cond ejecuta un flujo de control dependiendo de la condición si es cierta o no. Es algo parecido al switch de C y Java  pero con la diferencia que en este caso lo que se pone en los case son condiciones lógicas y no constantes.
+        //La palabra cond ejecuta un flujo de control dependiendo de la condiciÃ³n si es cierta o no. Es algo parecido al switch de C y Java  pero con la diferencia que en este caso lo que se pone en los case son condiciones lÃ³gicas y no constantes.
         public static final String COND = "cond";
-        //Añade al principio de una lista dada un elemento
+        //AÃ±ade al principio de una lista dada un elemento
         //(cons elem lista) 
         public static final String CONS = "cons";
-        //Define una función con un nombre el cual será llamado mediante el comando (nombre-funcion args) y que tiene como argumentos una lista de variables.
-        //El cuerpo es una lista que define qué hace la función y es ésta la que ejecuta la acción. 
+        //Define una funciÃ³n con un nombre el cual serÃ¡ llamado mediante el comando (nombre-funcion args) y que tiene como argumentos una lista de variables.
+        //El cuerpo es una lista que define quÃ© hace la funciÃ³n y es Ã©sta la que ejecuta la acciÃ³n. 
         public static final String DEFUN = "defun";
-        //Cierto (T, de true) si la lista está vacía, y nil (sin valor) en caso contrario
+        //Cierto (T, de true) si la lista estÃ¡ vacÃ­a, y nil (sin valor) en caso contrario
         public static final String ENDP = "endp";
-        //Se utiliza eq para saber si dos átomos son iguales, independientemente de la referencia.
+        //Se utiliza eq para saber si dos Ã¡tomos son iguales, independientemente de la referencia.
         //Sintaxis (eq X Y) 
         public static final String EQ = "eq";
         //Es utilizado equal para saber si dos listas son iguales, independientemente de la referencia. 
@@ -32,6 +32,6 @@ class Patrones_Lexer{
         //Construye una lista a partir de sus elementos
         //(list elem1 elem2 elem3 .. elemN) 
         public static final String LIST = "list";
-        //La función quote evita la evaluación de su argumento
+        //La funciÃ³n quote evita la evaluaciÃ³n de su argumento
         public static final String QUOTE= "quote"; 
 }
